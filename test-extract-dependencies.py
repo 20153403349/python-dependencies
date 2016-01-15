1c5630398c00fe4e744deca7ac57f9a23ebf6b53
Juan Nunez-Iglesias <juan.n@unimelb.edu.au>
2016-01-16 02:05:37 +1100

    Remove unused kwarg in tests

--- test-extract-dependencies.py
+++ test-extract-dependencies.py
@@ -5,6 +5,5 @@ import random
 client = xmlrpclib.ServerProxy('http://pypi.python.org/pypi')
 packages = ['gala', 'scikit-learn', 'scipy', 'scikit-image', 'Flask']
 random.shuffle(packages)
-for i, package in enumerate(packages):
-    extract_package(package, to='test-pypi-deps.txt',
-                    client=client, n=i)
+for package in packages:
+    extract_package(package, to='test-pypi-deps.txt', client=client)
